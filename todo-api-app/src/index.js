884e717e0d2764c670460e03e492f973a3b4f7b7
Me_Amey <padkanthwaramey@gmail.com>
2026-03-19 12:12:28 +0530

    updated

--- todo-api-app/src/index.js
+++ todo-api-app/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.css'; // Optional: if you want global styles
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Selecting the 'root' div from your public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
